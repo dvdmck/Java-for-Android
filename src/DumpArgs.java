5a1507747fd7926c25adf15f36dee78ce764b1e0
dvdmck <dperrez@gmail.com>
2011-02-04 14:00:12 +0100

    Ejemplo 1.1

---
+++ src/DumpArgs.java
@@ -0,0 +1,9 @@
+public class DumpArgs
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Argumentos: ");
+		for (int i=0;i<args.length;i++)
+			System.out.println(args[i]);
+	}
+}
